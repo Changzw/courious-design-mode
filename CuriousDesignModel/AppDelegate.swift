6b28f4bb72ad23805b876adc7e10ec1b8260dd39
czw <changzhongwei3@163.com>
2021-11-30 23:20:55 +0800

    init

---
+++ CuriousDesignModel/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  CuriousDesignModel
+//
+//  Created by 常仲伟 on 2021/11/30.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
+
---
+++ CuriousDesignModel/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CuriousDesignModel
+//
+//  Created by 常仲伟 on 2021/11/30.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+}
